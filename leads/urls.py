c3c0eb9c3c24d87a7b43a4cdab9fbe277afa01be
CeeSiama <cee.siama@gmail.com>
2022-02-23 23:19:34 +0530

    added CRUD functionalities

---
+++ leads/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+
+from leads.views import lead_create, lead_delete, lead_detail, lead_list, lead_update
+
+app_name = 'leads'
+urlpatterns = [
+    path('', lead_list),
+    path('<int:pk>/', lead_detail),
+    path('<int:pk>/update/', lead_update),
+    path('<int:pk>/delete/', lead_delete),
+    path('create/', lead_create),
+]
--- leads/views.py
+++ leads/views.py
@@ -1,3 +1,50 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import redirect, render, redirect
+from .models import Agent, Lead
+from .forms import LeadForm, LeadModelForm
 
-# Create your views here.
+
+def lead_list(request):
+    leads = Lead.objects.all()
+    context = {
+        "leads": leads,
+    }
+    return render(request, "leads/lead_list.html", context)
+
+
+def lead_detail(request, pk):
+    lead = Lead.objects.get(id=pk)
+    context = {
+        "lead": lead,
+    }
+    return render(request, "leads/lead_detail.html", context)
+
+
+def lead_create(request):
+    form = LeadModelForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        return redirect("/leads")
+    context = {
+        "form": form,
+    }
+    return render(request, "leads/lead_create.html", context)
+
+
+def lead_update(request, pk):
+    lead = Lead.objects.get(id=pk)
+    form = LeadModelForm(request.POST or None, instance=lead)
+    if form.is_valid():
+        form.save()
+        return redirect('/leads')
+    context = {
+        "lead": lead,
+        "form": form,
+    }
+    return render(request, "leads/lead_update.html", context)
+
+
+def lead_delete(request, pk):
+    lead = Lead.objects.get(id=pk)
+    lead.delete()
+    return redirect("/leads")
