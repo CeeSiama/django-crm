2eb6ecb61ce34e37ed8d15cc743c9903ff83d349
CeeSiama <cee.siama@gmail.com>
2022-02-24 23:22:39 +0530

    CRUD change to Class Base View and added static folder+settings

--- leads/urls.py
+++ leads/urls.py
@@ -1,12 +1,24 @@
+from unicodedata import name
 from django.urls import path
 
-from leads.views import lead_create, lead_delete, lead_detail, lead_list, lead_update
+from leads.views import (
+    lead_create,
+    lead_delete,
+    lead_detail,
+    lead_list,
+    lead_update,
+    LeadListView,
+    LeadDetailView,
+    LeadCreateView,
+    LeadUpdateView,
+    LeadDeleteView,
+)
 
 app_name = 'leads'
 urlpatterns = [
-    path('', lead_list),
-    path('<int:pk>/', lead_detail),
-    path('<int:pk>/update/', lead_update),
-    path('<int:pk>/delete/', lead_delete),
-    path('create/', lead_create),
+    path('', LeadListView.as_view(), name='lead-list'),
+    path('<int:pk>/', LeadDetailView.as_view(), name='lead-detail'),
+    path('<int:pk>/update/', LeadUpdateView.as_view(), name='lead-update'),
+    path('<int:pk>/delete/', LeadDeleteView.as_view(), name='lead-delete'),
+    path('create/', LeadCreateView.as_view(), name='lead-create'),
 ]
--- leads/views.py
+++ leads/views.py
@@ -1,7 +1,64 @@
-from django.http import HttpResponse
-from django.shortcuts import redirect, render, redirect
-from .models import Agent, Lead
-from .forms import LeadForm, LeadModelForm
+from django.core.mail import send_mail
+from django.shortcuts import redirect, render, reverse
+from django.views import generic
+from .models import Lead
+from .forms import LeadModelForm
+
+
+class LandingPageView(generic.TemplateView):
+    template_name = "landing.html"
+
+
+class LeadListView(generic.ListView):
+    template_name = "leads/lead_list.html"
+    queryset = Lead.objects.all()
+    context_object_name = 'leads'
+
+
+class LeadDetailView(generic.DetailView):
+    template_name = "leads/lead_detail.html"
+    queryset = Lead.objects.all()
+    context_object_name = 'lead'
+
+
+class LeadCreateView(generic.CreateView):
+    template_name = "leads/lead_create.html"
+    form_class = LeadModelForm
+
+    def get_success_url(self):
+        return reverse("leads:lead-list")
+
+    def form_valid(self, form):
+        # ToDo -> send email
+        send_mail(
+            subject="A lead has been created",
+            message="Go to the site to see the new lead",
+            from_email="test@test.com",
+            recipient_list=["test2@test.com"],
+        )
+        return super(LeadCreateView, self).form_valid(form)
+
+
+class LeadUpdateView(generic.UpdateView):
+    template_name = "leads/lead_update.html"
+    queryset = Lead.objects.all()
+    form_class = LeadModelForm
+
+    def get_success_url(self):
+        return reverse("leads:lead-list")
+
+
+class LeadDeleteView(generic.DeleteView):
+    template_name = "leads/lead_delete.html"
+    queryset = Lead.objects.all()
+
+    def get_success_url(self):
+        return reverse("leads:lead-list")
+
+
+# Function Base CRUD
+def landing_page(request):
+    return render(request, 'landing.html')
 
 
 def lead_list(request):
