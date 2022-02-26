1a6efe4fa365f4ae19c64cf23b2159b0b275f1b7
CeeSiama <cee.siama@gmail.com>
2022-02-26 23:40:52 +0530

    agent crud+filtering

---
+++ agents/views.py
@@ -0,0 +1,65 @@
+import imp
+from pydoc import classname
+from django.shortcuts import reverse, render
+from django.views import generic
+from django.contrib.auth.mixins import LoginRequiredMixin
+from .forms import AgentModelForm
+
+from leads.models import Agent
+
+
+class AgentListView(LoginRequiredMixin, generic.ListView):
+    template_name = "agents/agent_list.html"
+    context_object_name = "agents"
+
+    def get_queryset(self):
+        organisation = self.request.user.userprofile
+        return Agent.objects.filter(organisation=organisation)
+
+
+class AgentCreateView(LoginRequiredMixin, generic.CreateView):
+    template_name = "agents/agent_create.html"
+    form_class = AgentModelForm
+
+    def get_success_url(self):
+        return reverse("agents:agent-list")
+
+    def form_valid(self, form):
+        agent = form.save(commit=False)
+        agent.organisation = self.request.user.userprofile
+        agent.save()
+        return super(AgentCreateView, self).form_valid(form)
+
+
+class AgentDetailView(LoginRequiredMixin, generic.DetailView):
+    template_name = "agents/agent_detail.html"
+    context_object_name = "agent"
+
+    def get_queryset(self):
+        organisation = self.request.user.userprofile
+        return Agent.objects.filter(organisation=organisation)
+
+
+class AgentUpdateView(LoginRequiredMixin, generic.UpdateView):
+    template_name = "agents/agent_update.html"
+    form_class = AgentModelForm
+    context_object_name = "agent"
+
+    def get_queryset(self):
+        organisation = self.request.user.userprofile
+        return Agent.objects.filter(organisation=organisation)
+
+    def get_success_url(self):
+        return reverse("agents:agent-list")
+
+
+class AgentDeleteView(LoginRequiredMixin, generic.DeleteView):
+    template_name = "agents/agent_delete.html"
+    context_object_name = "agent"
+
+    def get_queryset(self):
+        organisation = self.request.user.userprofile
+        return Agent.objects.filter(organisation=organisation)
+
+    def get_success_url(self):
+        return reverse("agents:agent-list")
